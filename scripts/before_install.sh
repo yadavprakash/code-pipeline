474bb200a4821668b1b54996eab8046ef7bab6ef
sohanyadav <sohanyadavdevops@gmail.com>
2023-10-13 13:36:06 +0530

    update s3 name

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,7 +4,7 @@
 cd /home/ec2-user/server
 
 #_Update_&_Set_Node_Version
-curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
+curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
 
 #_Download_Node_&NPM
 yum -y install nodejs npm
